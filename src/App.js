460ebb4cb78f3b0d7d510be180db9825b24bd915
Svetlana Levkovskaya <levkovskayase@gmail.com>
2022-11-21 15:07:57 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -3,22 +3,38 @@ import "./App.css";
 const App = () => {
   return (
     <div>
-      <div>First div</div>
-      <div className="App">
-        <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>js</li>
-          <li>react</li>
-        </ul>
-        <div>Second div</div>
-        <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>js</li>
-          <li>react</li>
-        </ul>
-      </div>
+      <Header />
+      <Technologies />
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#s">Home</a>
+      <a href="#s">News Feed</a>
+      <a href="#s">Messages</a>
+    </div>
+  );
+};
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+      <div>Second div</div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
     </div>
   );
 };
