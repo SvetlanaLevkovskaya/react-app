d3c1ec6cc4e699d8eba808994105321b7efdb5f6
Svetlana Levkovskaya <levkovskayase@gmail.com>
2022-11-21 15:49:59 +0300

    improvements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
+import Footer from "./Footer";
 import Header from "./Header";
 import Technologies from "./Technolodies";
 
 const App = () => {
   return (
     <div>
-      <Header />
+      <Header/>
       <Technologies />
+      <Footer/>
     </div>
   );
 };
