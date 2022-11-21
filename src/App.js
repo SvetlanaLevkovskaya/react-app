7a02ebc179c65faccfd45a893dc1df67ea9cb2cf
Svetlana Levkovskaya <levkovskayase@gmail.com>
2022-11-21 15:31:13 +0300

    add import components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
+import React from "react";
 import "./App.css";
+import Header from "./Header";
+import Technologies from "./Technolodies";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Technologies />
-    </div>
-  );
-};
-
-const Header = () => {
-  return (
-    <div>
-      <a href="#s">Home</a>
-      <a href="#s">News Feed</a>
-      <a href="#s">Messages</a>
-    </div>
-  );
-};
-
-const Technologies = () => {
-  return (
-    <div>
-      <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
-      <div>Second div</div>
-      <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
     </div>
   );
 };
