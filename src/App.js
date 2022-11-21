22e741146b1fe19e951e053d36ad85f493c4478e
Svetlana Levkovskaya <levkovskayase@gmail.com>
2022-11-21 14:14:14 +0300

    create react app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <div>First div</div>
+      <div className="App">
+        <ul>
+          <li>css</li>
+          <li>html</li>
+          <li>js</li>
+          <li>react</li>
+        </ul>
+        <div>Second div</div>
+        <ul>
+          <li>css</li>
+          <li>html</li>
+          <li>js</li>
+          <li>react</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
