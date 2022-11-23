dde47d11c78afb38da198ffb12209807d789736f
Svetlana Levkovskaya <levkovskayase@gmail.com>
2022-11-23 14:17:19 +0300

    add styles to dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,35 @@
 import React from "react"
-//import styles from "./Dialogs.module.css"
+import styles from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
     return (
-        <div>Dialogs</div>
+        <div className={styles.dialogs}>
+          <div className={styles.dialogsItems + " " + styles.active}>
+            <div className={styles.dialog}>
+              Sveta
+            </div>
+            <div className={styles.dialog}>
+              Viktor
+            </div>
+            <div className={styles.dialog}>
+              Vadim
+            </div>
+            <div className={styles.dialog}>
+              Andrey
+            </div>
+            <div className={styles.dialog}>
+              Gleb
+            </div>
+            <div className={styles.dialog}>
+              Sasha
+            </div>
+          </div>
+          <div className={styles.messages}>
+            <div className={styles.message}>Hello</div>
+            <div className={styles.message}>Hi</div>
+            <div className={styles.message}>How are you?</div>
+          </div>
+        </div>
     )
 }
 
